335d5a81d9291cfaf1f08998431d8d4e0a74df22
vassilismourikis <vassilismourikis@gmail.com>
2022-04-01 14:15:56 +0300

    init

---
+++ src/main/java/com/example/demo/Answer.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class Answer {
+    private String desc;
+    private boolean isCorrect;
+
+    public Answer(String desc, boolean isCorrect) {
+        this.desc = desc;
+        this.isCorrect = isCorrect;
+    }
+}
